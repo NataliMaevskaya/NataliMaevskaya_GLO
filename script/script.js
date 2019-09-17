d0b5d9531ee9a79579e7877693194f7e7604c0e5
Natali.Maevskaya <natali.maevskaya@gmail.com>
2019-09-17 18:35:09 +0300

    Практика Слайдер - обязательное

--- script/script.js
+++ script/script.js
@@ -8,8 +8,28 @@ window.addEventListener('DOMContentLoaded', function() {
             
         function getTimeRemaining() {
             // secs += 1;
+            
+
+                // const convertGMTtoUTC = (date) => {
+                //     const strUTC = date.toISOString();
+                //     console.log('strUTC: ', strUTC);
+                //     // console.log(strUTC.substr(0, 4), strUTC.substr(5, 7), strUTC.substr(8, 10), strUTC.substr(11, 13), strUTC.substr(14, 16), strUTC.substr(17, 19));
+                //     // console.log('день: ', strUTC.substr(8, 10));
+                //     // console.log('месяц: ', strUTC.substr(5, 7));
+                //     // console.log('год: ', strUTC.substr(0, 4));
+                    
+                //     return new Date(strUTC.substr(0, 4), strUTC.substr(5, 7), strUTC.substr(8, 10), strUTC.substr(11, 13), strUTC.substr(14, 16), strUTC.substr(17, 19));
+                // };
+                // const deadlineStrUTC = convertGMTtoUTC(deadline);
+                // const deadlineStrUTC = convertGMTtoUTC(deadline),
+                // dateNowStrUTC = convertGMTtoUTC(new Date());
+                // console.log(deadlineStrUTC);
+                // console.log(dateNowStrUTC);
+//                 2019-09-16T21:00:00.000Z
+// s            14 2019-09-16T14:38:22.858Z
             let dateStop = new Date(deadline).getTime(),
-                // dateStop = new Date(deadline.getUTCFullYear(), deadline.getUTCMonth(), deadline.getUTCDate(), deadline.getUTCHours(), deadline.getUTCMinutes(), deadline.getUTCSeconds()).getTime(),            
+            // let    dateStop = new Date(deadline.getUTCFullYear(), deadline.getUTCMonth(), deadline.getUTCDate(), deadline.getUTCHours(), deadline.getUTCMinutes(), deadline.getUTCSeconds()).getTime(),            
+            
                 dateNow = new Date(),
                 // now = new Date(),
                 // now.setHours(23,59,secs),
@@ -19,6 +39,7 @@ window.addEventListener('DOMContentLoaded', function() {
                 minutes = Math.floor((timeRemaining / 60) % 60),
                 hours = Math.floor(timeRemaining / 60 / 60);
 
+                // console.log('dateStop: ', deadline.getUTCFullYear(), deadline.getUTCMonth(), deadline.getUTCDate(), deadline.getUTCHours(), deadline.getUTCMinutes(), deadline.getUTCSeconds());
                 return {timeRemaining, hours, minutes, seconds};
                 
         }
@@ -38,7 +59,7 @@ window.addEventListener('DOMContentLoaded', function() {
             timerSeconds.textContent = timer.seconds;
 
             if(timer.timeRemaining > 0) {
-                setInterval(updateClock, 1000);
+                setTimeout(updateClock, 1000);
             } 
             // if (timer.timeRemaining === 0) {
             //     let date = new Date();
@@ -60,8 +81,10 @@ window.addEventListener('DOMContentLoaded', function() {
     let date = new Date();
     
     date.setHours(0, 0, 0, 0);
+    // date.setHours(date.getHours() - 3, 0, 0, 0);
     // date.setDate(date.getDate() - 1);
     date.setDate(date.getDate() + 1);
+    // console.log(date);
     // let secs = 0;
     countTimer(date);
 
@@ -78,31 +101,18 @@ window.addEventListener('DOMContentLoaded', function() {
                 menu.style.transform = `translate(-100%)`;
             }
         };
-        body.addEventListener('click', () => {
-            let target = event.target;
+        body.addEventListener('click', (event) => {
+            const target = event.target;
             // console.log(target);
             // if((target.closest('menu') && !target.matches('menu')) || (target.closest('.menu') && !target.matches('.menu'))) handlerMenu();
-            if((target.closest('menu') && !target.matches('menu')) || target.closest('.menu')) handlerMenu();
+            if((target.closest('menu') && !target.matches('menu')) || target.closest('.menu')) {
+                handlerMenu(); 
+            }
             else if (target.closest('body') && !target.matches('menu')) {
                 menu.style.transform = `translate(-100%)`;
             }
             
-        });
-        // menu.addEventListener('click', (event) => {
-        //     let target = event.target;
-        //     target = target.closest('menu');
-        //     handlerMenu();
-        //     // if (target) {
-        //     //     handlerMenu();
-        //     // } else {
-        //     //     handlerMenu();
-        //     // }
-            
-        // }); 
-
-        // btnMenu.addEventListener('click', handlerMenu);
-            
-        
+        });       
     };
     toggleMenu();
     
@@ -287,4 +297,101 @@ window.addEventListener('DOMContentLoaded', function() {
         });
     };
     tabs();    
+
+    //слайдер 
+    const slider = () => {
+        const slide  = document.querySelectorAll('.portfolio-item'),
+            btn = document.querySelectorAll('.portfolio-btn'),
+            portfolioDots = document.querySelector('.portfolio-dots'),
+            slider = document.querySelector('.portfolio-content');
+
+            
+        const addDots = (numDots) => {
+            for (let i = 0; i < numDots; i++) {
+                let newElem = document.createElement('li');
+                newElem.classList.add('dot');
+                if (i === 0) {
+                    newElem.classList.add('dot-active');
+                }
+                portfolioDots.appendChild(newElem);
+            }
+        };
+        addDots(slide.length);
+
+        const dot = document.querySelectorAll('.dot');
+        let currentSlide = 0,
+            interval;
+
+        const prevSlide = (elem, index, strClass) => {
+            elem[index].classList.remove(strClass);
+        };
+
+        const nextSlide = (elem, index, strClass) => {
+            elem[index].classList.add(strClass);
+        };
+
+        const autoPlaySlide = () => {
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+            currentSlide++;
+            if (currentSlide >= slide.length) {
+                currentSlide = 0;
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+        };
+
+        const startSlide = (time = 3000) => {
+            interval = setInterval(autoPlaySlide, time);
+        };
+
+        const stopSlide = () => {
+            clearInterval(interval);
+        };
+        slider.addEventListener('click', (event) => {
+            event.preventDefault();
+            let target = event.target;
+
+            if(!target.matches('.portfolio-btn, .dot')) {
+                return;
+            }
+
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+
+            if (target.matches('#arrow-right')) {
+                currentSlide++;
+            } else if (target.matches('#arrow-left')) {
+                currentSlide--;
+            } else if (target.matches('.dot')) {
+                dot.forEach((elem, index) => {
+                    if (elem === target) {
+                        currentSlide = index;
+                    }
+                });
+            }
+            if (currentSlide >= slide.length) {
+                currentSlide = 0;
+            }
+            if (currentSlide < 0) {
+                currentSlide = slide.length - 1;
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+        });
+        slider.addEventListener ('mouseover', (event) => {
+            if (event.target.matches('.portfolio-btn') ||
+            event.target.matches('.dot')) {
+                stopSlide();
+            }
+        });
+        slider.addEventListener ('mouseout', (event) => {
+            if (event.target.matches('.portfolio-btn') ||
+            event.target.matches('.dot')) {
+                startSlide();
+            }
+        });
+        startSlide(1500);
+    };
+    slider();
 });
